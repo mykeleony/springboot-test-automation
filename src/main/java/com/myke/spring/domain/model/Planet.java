3d0812caf07150ca722d1a9bd14a493a7e7cbdc0
myke amorim <myke.amorim@usp.br>
2023-02-13 22:02:24 -0300

    Created planet entity

---
+++ src/main/java/com/myke/spring/domain/model/Planet.java
@@ -0,0 +1,40 @@
+package com.myke.spring.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity(name = "planets")
+@NoArgsConstructor
+public class Planet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String climate;
+    private String terrain;
+
+    public Planet(String name, String climate, String terrain) {
+        this.name = name;
+        this.climate = climate;
+        this.terrain = terrain;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Planet planet)) return false;
+        return getId().equals(planet.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
