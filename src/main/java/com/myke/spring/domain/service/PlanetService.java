908d3e9868143dd71ec17412052569ac227497ac
myke amorim <myke.amorim@usp.br>
2023-02-15 14:14:04 -0300

    Added query and delete functions

--- src/main/java/com/myke/spring/domain/service/PlanetService.java
+++ src/main/java/com/myke/spring/domain/service/PlanetService.java
@@ -4,6 +4,10 @@ import com.myke.spring.domain.model.Planet;
 import com.myke.spring.domain.repository.PlanetRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -15,4 +19,19 @@ public class PlanetService {
         return planetRepository.save(planet);
     }
 
+    public Optional<Planet> get(Long id) {
+        return planetRepository.findById(id);
+    }
+
+    public Optional<Planet> getByName(String name) {
+         return planetRepository.findByName(name);
+    }
+
+    public List<Planet> findAll() {
+        return planetRepository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        planetRepository.deleteById(id);
+    }
 }
