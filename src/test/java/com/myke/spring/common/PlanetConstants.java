ab87d2b0b1c35fca843b490ff776936f29c02da1
myke amorim <myke.amorim@usp.br>
2023-02-15 03:06:36 -0300

    Added invalid planet constant

--- src/test/java/com/myke/spring/common/PlanetConstants.java
+++ src/test/java/com/myke/spring/common/PlanetConstants.java
@@ -4,4 +4,5 @@ import com.myke.spring.domain.model.Planet;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("1st planet", "Arid", "Desert");
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
 }
