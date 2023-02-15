2c579474cc51de997f2de59e7926b27b1e18a1b5
myke amorim <myke.amorim@usp.br>
2023-02-15 02:50:22 -0300

    Concluded planet creation with valid data test

--- src/test/java/com/myke/spring/domain/PlanetServiceTest.java
+++ src/test/java/com/myke/spring/domain/PlanetServiceTest.java
@@ -1,22 +1,33 @@
 package com.myke.spring.domain;
 
 import com.myke.spring.domain.model.Planet;
+import com.myke.spring.domain.repository.PlanetRepository;
 import com.myke.spring.domain.service.PlanetService;
-import lombok.AllArgsConstructor;
-import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import static com.myke.spring.common.PlanetConstants.PLANET;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest(classes = PlanetService.class)
-@AllArgsConstructor
+import static com.myke.spring.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class PlanetServiceTest {
 
+    @InjectMocks
     private PlanetService planetService;
 
+    @Mock
+    private PlanetRepository planetRepository;
+
     // Naming schema: operation_state_expectedOutput
+    // AAA testing -> Arrange, Act, Assert
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
         // System Under Test
         Planet sut = planetService.create(PLANET);
 
