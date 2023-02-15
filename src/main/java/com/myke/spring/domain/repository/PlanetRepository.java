b2702a34f38f7bd44e7bc21633500730e84f130b
myke amorim <myke.amorim@usp.br>
2023-02-15 14:13:19 -0300

    Added name search feature

--- src/main/java/com/myke/spring/domain/repository/PlanetRepository.java
+++ src/main/java/com/myke/spring/domain/repository/PlanetRepository.java
@@ -4,7 +4,9 @@ import com.myke.spring.domain.model.Planet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
-
+    Optional<Planet> findByName(String name);
 }
