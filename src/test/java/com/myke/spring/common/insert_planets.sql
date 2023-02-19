bbaad4670765052e7272e99f761d8b4e71621746
myke amorim <myke.amorim@usp.br>
2023-02-19 15:47:12 -0300

    Created planet insertion SQL script

---
+++ src/test/java/com/myke/spring/common/insert_planets.sql
@@ -0,0 +1,3 @@
+INSERT INTO planets (id, name, climate, terrain) VALUES (1, 'Tatooine', 'arid', 'desert');
+INSERT INTO planets (id, name, climate, terrain) VALUES (2, 'Alderaan', 'Temperate', 'Grasslands, Mountains');
+INSERT INTO planets (id, name, climate, terrain) VALUES (3, 'Yavin IV', 'Temperate, tropical', 'Jungle, rainforest');
