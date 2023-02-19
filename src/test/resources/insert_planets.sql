4041ab732e4d93248a58d82f2e88398c182d507a
myke amorim <myke.amorim@usp.br>
2023-02-19 16:48:19 -0300

    Fixed lower case field

--- src/test/resources/insert_planets.sql
+++ src/test/resources/insert_planets.sql
@@ -1,3 +1,3 @@
-INSERT INTO planets (id, name, climate, terrain) VALUES (1, 'Tatooine', 'arid', 'desert');
+INSERT INTO planets (id, name, climate, terrain) VALUES (1, 'Tatooine', 'Arid', 'Desert');
 INSERT INTO planets (id, name, climate, terrain) VALUES (2, 'Alderaan', 'Temperate', 'Grasslands, Mountains');
 INSERT INTO planets (id, name, climate, terrain) VALUES (3, 'Yavin IV', 'Temperate, tropical', 'Jungle, rainforest');
