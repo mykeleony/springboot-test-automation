dde83164b423cdee28b4a410c38ab0961ea4dffb
myke amorim <myke.amorim@usp.br>
2023-02-18 22:52:17 -0300

    Removed space from planet name

--- src/test/java/com/myke/spring/common/PlanetConstants.java
+++ src/test/java/com/myke/spring/common/PlanetConstants.java
@@ -3,6 +3,6 @@ package com.myke.spring.common;
 import com.myke.spring.domain.model.Planet;
 
 public class PlanetConstants {
-    public static final Planet PLANET = new Planet("1st planet", "Arid", "Desert");
+    public static final Planet PLANET = new Planet("Planet", "Arid", "Desert");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
 }
