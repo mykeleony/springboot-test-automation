612a7d4119c06c501c277e566ecdd63beac36fbb
myke amorim <myke.amorim@usp.br>
2023-02-19 15:47:41 -0300

    Added planets list constant

--- src/test/java/com/myke/spring/common/PlanetConstants.java
+++ src/test/java/com/myke/spring/common/PlanetConstants.java
@@ -1,8 +1,24 @@
 package com.myke.spring.common;
 
 import com.myke.spring.domain.model.Planet;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("Planet", "Arid", "Desert");
     public static final Planet INVALID_PLANET = new Planet("", "", "");
+
+    public static final Planet TATOOINE = new Planet(1L, "Tatooine", "Arid", "Desert");
+    public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "Temperate", "Grasslands, mountains");
+    public static final Planet YAVINIV = new Planet(3L, "Yavin IV", "Temperate, tropical", "Jungle, rainforest");
+
+    public static final List<Planet> PLANETS = new ArrayList<>() {
+        {
+            add(TATOOINE);
+            add(ALDERAAN);
+            add(YAVINIV);
+        }
+    };
 }
